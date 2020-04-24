051bbe09eccdb9a38e0ad25e502eb5b7f5a73458
darstih <48297481+darstih@users.noreply.github.com>
2020-04-24 16:40:38 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,71 @@
+# Extraer publicaciones de grupo privado de facebook
+
+
+
+## Uso
+Usar esta función cuando requiera hacer una extracción de las publicaciones sin los comentarios de un grupo de facebook privado del cual tenga una cuenta con acceso a este
+
+## extraer_publicaciones_grupo_privado
+```
+extraer_publicaciones_grupo_privado(user,pwd,iteraciones,archivo_final,id_grupo="",ultima_pagina='',mostrar_autor=True,mostrar_iteracion_proceso = True,archivo_proceso='')
+```
+
+
+## Parametros
+- user: Usuario de la cuenta de facebook que tiene acceso al grupo
+- pwd: Contraseña de la cuenta
+- iteraciones: cantidad de paginas a las cuales va a entrar el algoritmo
+- archivo_final: ruta del archivo en formato csv o txt en la cual se escribiran los datos extraidos
+- id_grupo: identificador del grupo objetivo
+- archivo proceso: (opcional) si se desea guardar los datos del proceso ingrese esta variable como la ruta donde se guardarán los datos del proceso al finalizar la ejecución
+- ultima_pagina: (opcional) si usted ya ejecutó una primera extracción con las iteraciones insuficientes podrá continuar escribiendo aquí la pagina siguiente de los datos de proceso
+- mostrar_autor: (opcional) si desea observar en consola los autores de cada publicación escrita a medida que se extraen los datos
+- mostrar_iteracion_proceso: (opcional) si desea observar en consola el número de iteraciones a medida que se cargan
+
+
+## Salida
+Al finalizar la ejecución se mostrará por pantalla y/o se guardarán los datos del proceso
+Hora de inicio, tiempo de ejecución, ultima iteración, siguiente pagina a visitar la cual se podrá usar para continuar más adelante la extracción
+
+En el archivo final podra ver los datos de cada publicación  
+id,autor,usuario_autor,texto,imagen,imagen_content,fecha,num_reacciones,num_comentarios
+
+La fecha capturada en las primeras publicaciones será la fecha a partir de la hora de tomada estos datos (fecha actual - fecha capturada = fecha publicacion)
+
+Capturará publicaciones con identificador 0, que significa en realidad que son publicaciones externas al  grupo que un usuario compartió en este
+
+El texto que captura que contenga comas y/o saltos de linea serán reemplazadas por <coma> y/o <enter> respectivamente
+
+
+
+### Ejemplo
+usuario = user
+password = password
+iteraciones = 10000
+id_grupo = 1
+archivo final = publicaciones.csv
+```
+extraer_publicaciones_grupo_privado(user = 'user',pwd='password',iteraciones=10000,id_grupo="149144201830763",archivo_final='publicaciones.csv',archivo_proceso='datosproceso.txt')
+```
+
+
+## Descripción
+El codigo aquí implementado contiene una función que hace una extracción de datos de un grupo privado del cual el usuario es miembro, saltando de esta forma la mala calidad que nos brinda la API de facebook.
+
+## Características
+* Se usa la libreria selenium de python [Documentación selenium](https://selenium-python.readthedocs.io/)
+* Por lo anterior abrira un navegador web donde simulará una la vista de un usuario
+* El algoritmo escribirá en disco cada publicación
+* El algoritmo usará la antigua mobile.facebook.com para minimizar el uso de red
+* Podrá descargar gran cantidad de estos datos
+* Tiene una velocidad aproximada de #### publicaciones por hora
+
+## Requerimientos
+* python 3
+* libreria selenium
+* descargar el geckodriver de la libreria según el navegador que usará para este proposito
+* conexión a internet(obviamente)
+
+## Desventajas 
+* Por evitar que facebook identifique el algoritmo como robot, este debe hacer ciertas pausas para "Humanizar" el comportamiento, por lo tanto, gastará bastante tiempo
+* Dado que usamos mobile.facebook.com el algoritmo no podrá obtener los comentarios, sin embargo, al obtener el id de la publicación se puede hacer un scraping horizontal iterando por cada uno de estos
---
+++ extraer_publicaciones_grupo_privado.py
@@ -0,0 +1,138 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from time import time
+import random
+import datetime as dt
+
+
+def extraer_publicaciones_grupo_privado(user,pwd,iteraciones,archivo_final,id_grupo="",ultima_pagina='',mostrar_autor=True,mostrar_iteracion_proceso = True,archivo_proceso=''):
+    driver = webdriver.Firefox(executable_path=r'/etc/geckodriver/geckodriver') #Busca el controlador asociado a la libreria
+
+
+    driver.get("https://mobile.facebook.com/") # Nos dirigimos a la pagina de facebook
+
+    #Proceso de login
+    elem = driver.find_element_by_id("m_login_email") 
+    elem.send_keys(user)
+    elem = driver.find_element_by_xpath('/html/body/div/div/div[2]/div/table/tbody/tr/td/div[2]/div[2]/form/ul/li[2]/section/input')
+    elem.send_keys(pwd)
+    elem.send_keys(Keys.RETURN)
+    if ultima_pagina =="":
+        assert id_grupo != "", "No existen los parametros necesesarios para ejecutar"
+        ultima_pagina = 'https://mobile.facebook.com/groups/'+id_grupo+'/'
+    start = time()    
+
+    
+    #Función para capturar las publicaciones de cada pagina cargada
+    #Tratar con errores nos ayudará a clasificar la información y a que nuestro spider no "caiga"
+
+    def capturarDatos():
+        
+        #Extracción autor
+        try:
+            autorcont = k.find_element_by_xpath(
+                './/div/header/table/tbody/tr/td[2]/header/h3')
+            autor = autorcont.find_element_by_xpath('.//span/strong[1]/a')
+            link_autor = autor.get_attribute('href')
+            link_autor = link_autor[28:link_autor.find('?')]
+        except:
+            autor = None
+            link_autor = None
+        #Extracción texto publicación
+        try:
+            texto = k.find_element_by_xpath('.//div/div').text
+            texto = texto.replace('\n', "<enter>")
+            texto = texto.replace(',', " <coma>")
+        except:
+            texto = ""
+        #Extracción de imagen o recurso
+        try:
+            imagen = k.find_element_by_xpath('.//img[@class="t"]')
+            imagen_content = imagen.get_attribute('alt')
+            imagen = imagen.get_attribute('src')
+            imagen = imagen.replace('\n', "<enter>")
+            imagen = imagen.replace(',', " <coma>")
+            imagen_content = imagen_content.replace('\n', "<enter>")
+            imagen_content = imagen_content.replace(',', " <coma>")
+        except:
+            imagen = ""
+            imagen_content = ""
+        #Extracción de la fecha a partir de la hora de tomada estos datos (fecha actual - fecha capturada = fecha publicacion)
+        try:
+            fecha = k.find_element_by_xpath('.//footer/div[1]').text
+        except:
+            fecha = ""
+        #Extracción número de reacciones
+        try:
+            num_reacciones = k.find_element_by_xpath(
+                './/a[@class="eb ec"]').text
+        except:
+            num_reacciones = '0'
+        #Extracción número de comentarios
+        try:
+            num_comentarios = (k.find_element_by_xpath(
+                './/a[@class="ec"]').text).split(" ")[0]
+        except:
+            num_comentarios = '0'
+        #Extracción del identificador
+        try:
+            l = k.find_elements_by_xpath('.//footer/div[2]/a')
+            if len(l) == 4:
+                l = l[1].get_attribute('href')
+            else:
+                l = l[-2].get_attribute('href')
+            m = l.find('id=')
+            l = l[m+3:l.find('&', m)]
+        except:
+            l = '0'
+        #Escritura de los datos
+        #Para minimizar el uso de memoria abrimos y cerramos en cada pagina cargada el documento
+        f = open(archivo_final, 'a')
+        if autor is not None:
+            if mostrar_autor:
+                print('Publicación de '+autor.text)
+            
+        
+            f.write('\n' + l+','+autor.text+','+link_autor+','+texto+','+imagen +
+                    ','+imagen_content+','+fecha+','+num_reacciones+','+num_comentarios)
+        f.close()
+
+        
+
+    sleep(3)
+    #ingresamos al grupo o la ultima pagina que visitamos
+    driver.get(ultima_pagina)
+
+    #iteraciones sobre la pagina
+    # Número de iteración en la que va
+    iteracion_en_proceso = 0
+    siguiente_pagina = ""
+    
+    for i in range(0, iteraciones):
+        publicaciones = driver.find_elements_by_tag_name('article')[1:]
+        iteracion_en_proceso = i
+        if mostrar_iteracion_proceso:
+            print('Iteración N° '+str(iteracion_en_proceso))
+        for k in publicaciones:
+            capturarDatos()
+             
+        siguiente_pagina = driver.find_element_by_xpath('/html/body/div/div/div[2]/div/div[1]/div[4]/div/a').get_attribute('href')
+        driver.get(siguiente_pagina)
+        sleep(random.uniform(2.,4.))
+
+    #Se captura la ultima pagina y sus parametros
+    capturarDatos()
+    siguiente_pagina = driver.find_element_by_xpath('/html/body/div/div/div[2]/div/div[1]/div[4]/div/a').get_attribute('href')
+
+    #Calculo de los datos del proceso
+    end = time()
+    tiempo_ejecucion = end - start
+
+    mensaje = 'Fecha y Hora de inicio : '+str(dt.datetime.fromtimestamp(start))+'\nFecha y Hora de fin : '+str(dt.datetime.fromtimestamp(end))+"\nTiempo ejecución : "+str(tiempo_ejecucion)+'seg \nUltima iteración : '+str(iteracion_en_proceso)+'\nSiguiente pagina a visitar : '+str(siguiente_pagina)+'\n'
+    print(mensaje)
+    datosproceso = open ("datosproceso.txt", "w")
+    datosproceso.write(mensaje)
+    datosproceso.close()
+    driver.quit()
+
